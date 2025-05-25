ecfe2bd9a02713a20d74f9d6d56c920e876f67d0
Erick Landaverry <landaverry.dev@gmail.com>
2025-05-25 12:02:58 -0500

    Adds routes for SMS form and submission.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SmsController;
 
+// still keeping the default Laravel welcome page
 Route::get('/', function () {
     return view('welcome');
 });
+
+// shows the SMS form I built earlier
+Route::get('/send-sms', function () {
+    return view('send-sms');
+});
+
+// handles the form submit and actually sends the text
+Route::post('/send-sms', [SmsController::class, 'send'])->name('sms.send');
